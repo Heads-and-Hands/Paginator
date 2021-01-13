b5f1a952fae1dd3027760275eb646bdc23bdac12
EndoNazer <rodindaniil@gmail.com>
2021-01-13 12:51:37 +0400

    Fix isLoading variable in Paginator

--- Sources/Paginator/Paginator.swift
+++ Sources/Paginator/Paginator.swift
@@ -71,7 +71,7 @@ public class Paginator {
     }
 
     private var isLoading: Bool {
-        currentPageMeta.isFinished
+        !currentPageMeta.isFinished
     }
 
     private func prepare<Payload: PaginatorPagePayload>(
